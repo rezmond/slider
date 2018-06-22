05c17c029c28eda1780fbdc9c171f90526ea751a
rezolvent <maaaloli@gmail.com>
2018-06-22 16:00:54 +0300

    start making of slider

--- slider-component/src/App.js
+++ slider-component/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import { Slider } from './components/Slider/index.jsx'
+
 
 class App extends Component {
   render() {
@@ -10,9 +13,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <Slider/>
+        </div>
       </div>
     );
   }
---
+++ slider-component/src/components/Slider/index.jsx
@@ -0,0 +1,10 @@
+/* author: makarov */
+
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export function Slider () {
+    return (
+        <div>My slider</div>
+    );
+}
