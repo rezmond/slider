8eb893aec62713e371036881a43558f5c7339b27
rezolvent <maaaloli@gmail.com>
2018-06-25 15:23:29 +0300

    implement carousel base

--- slider-component/src/components/Slider/index.jsx
+++ slider-component/src/components/Slider/index.jsx
@@ -3,8 +3,70 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-export function Slider () {
-    return (
-        <div>My slider</div>
-    );
+import {Slide} from "./components/Slide";
+import "./style.css";
+
+export class Slider extends React.PureComponent {
+    static propTypes = {
+        url: PropTypes.string.isRequired,
+        duration: PropTypes.number,
+    };
+    static defaultProps = {
+        duration: 400,
+        width: 960,
+        height: 300,
+    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            index: 0,
+            carouselStyle: {
+                width: props.width,
+            }
+        };
+        this._slidesData = [{
+            text: "first slide",
+            header: "first",
+            url: "http://placekitten.com/400/200",
+        }, {
+            text: "second slide",
+            header: "second",
+            url: "http://placekitten.com/400/201",
+        }, {
+            text: "third slide",
+            header: "third",
+            url: "http://placekitten.com/400/202",
+        }];
+        this._slides = [...this._slidesData];
+    }
+    render() {
+        const {
+            width,
+            height,
+        } = this.props;
+        return (
+            <div className="b-carousel-container" style={{
+                width,
+                height,
+            }}>
+                <ul className="b-carousel" style={this.state.carouselStyle}>
+                    {
+                        this._slides.map((slide, i) => (
+                            <Slide
+                                {...slide}
+                                key={i + slide.url}
+                                offset={(width * i) - (this.state.index * width)}
+                                width={width}
+                                height={height}
+                            />
+                        ))
+                    }
+                </ul>
+                <div className="b-carousel__controls">
+                    <div className="b-carousel__arrow-left"></div>
+                    <div className="b-carousel__arrow-right"></div>
+                </div>
+            </div>
+        );
+    }
 }
